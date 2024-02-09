4820a13651bc367ac4c9a147c80b774ab066a292
MICHAEL KOFI NYANYUIE <mnyanyuie@gmail.com>
2024-02-09 21:47:15 +0100

    Exo 1 and 2 Complete

--- data_processing.r
+++ data_processing.r
@@ -1,10 +1,31 @@
 library(RMariaDB)
-# Assuming rstudioapi is installed. If not, you might need to install it using install.packages("rstudioapi")
+#install.packages("rstudioapi") -> TO BE INSTALLED
+
 password <- rstudioapi::askForPassword("knuMANJwJYSM2Nd1")
-con <- dbConnect(drv = RMariaDB::MariaDB(), 
-                 user = 'michael-kofi.nyanyui', 
-                 password = password, 
-                 dbname = '23_2L2_michael_kofi_nyanyuie', 
-                 host = '10.10.190.39',
-                 port = 3306
-				)
+connection <- dbConnect(
+  drv = RMariaDB::MariaDB(),
+  user = "michael-kofi.nyanyui",
+  password = password,
+  dbname = "23_2L2_michael_kofi_nyanyuie",
+  host = "10.10.190.39",
+  port = 3306
+)
+
+
+#Exo 4.2
+#Recupere les noms de tous les tables dans ma DB
+table_list <- dbListTables(connection)
+
+#Recupere les noms des columns
+department_columns <- dbListFields(connection, "DEPARTMENTS")
+
+#Recupere les tuples da la table DEPARTMENTS
+department_data <- dbReadTable(connection, "DEPARTMENTS")
+
+#Recupere les données de villes ayant plus de 300 000 habitants
+data_snippet <- dbGetQuery(connection, "SELECT * 
+  FROM DEPARTMENTS 
+  WHERE Population > 300000"
+)
+
+dbClearResult(data_snippet)
