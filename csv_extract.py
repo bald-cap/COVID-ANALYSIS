82c86436d87d418134f7af183284481879fecf02
MICHAEL KOFI NYANYUIE <mnyanyuie@gmail.com>
2024-03-09 20:23:32 +0100

    tried adding files to MariaDB directly from python file

--- csv_extract.py
+++ csv_extract.py
@@ -11,11 +11,15 @@ import sqlalchemy as sqlal
 #     newTuples.to_csv(f"temp2019_chunk{i+1}.csv", index=False)
 
 
-tempDF = pd.read_csv("temp2020/temp2020.csv")
+# tempDF = pd.read_csv("temp2020/temp2020.csv")
+tempDF = pd.read_csv("temp2021.csv")
+
 
 chunkSize = 2000
+connection = sqlal.create_engine('mysql+pymysql://michael-kofi.nyanyui:knuMANJwJYSM2Nd1@10.10.190.39/23_2L2_michael_kofi_nyanyuie')
 
 for i, chunk in enumerate(range(0, tempDF.shape[0], chunkSize)):
     newTuples = tempDF.iloc[chunk : chunk + chunkSize]
 
-    newTuples.to_csv(f"temp2020_chunk{i+1}.csv", index=False)
+    newTuples.to_csv(f"temp2021_chunk{i+1}.csv", index=False)
+    newTuples.to_sql('TEMPERATURES', con=connection, if_exists='replace', index=False)
