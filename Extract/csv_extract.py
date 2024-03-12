f0a032e7508c4e3f02175697b585b79a45617b9d
MICHAEL KOFI NYANYUIE <mnyanyuie@gmail.com>
2024-03-12 15:04:36 +0100

    added column names

--- Extract/csv_extract.py
+++ Extract/csv_extract.py
@@ -18,6 +18,7 @@ tempDF = pd.read_csv("temp2021.csv", sep=";")
 chunkSize = 2000
 connection = sqlal.create_engine('mysql+pymysql://michael-kofi.nyanyui:knuMANJwJYSM2Nd1@10.10.190.39/23_2L2_michael_kofi_nyanyuie')
 
+tempDF.columns = ['DATE_TEMP', 'DEP_ID', 'DEPARTMENT', 'MIN_TEMP', 'MAX_TEMP', 'MEAN_TEMP']
 for i, chunk in enumerate(range(0, tempDF.shape[0], chunkSize)):
     newTuples = tempDF.iloc[chunk : chunk + chunkSize]
 
