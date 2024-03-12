89c87a5d2fd919a67eb8d0cfccdbf0ebf7a0205e
MICHAEL KOFI NYANYUIE <mnyanyuie@gmail.com>
2024-03-12 13:50:42 +0100

    added 2021 chunks

--- Extract/csv_extract.py
+++ Extract/csv_extract.py
@@ -12,14 +12,14 @@ import sqlalchemy as sqlal
 
 
 # tempDF = pd.read_csv("temp2020/temp2020.csv")
-tempDF = pd.read_csv("temp2021.csv")
+tempDF = pd.read_csv("temp2021.csv", sep=";")
 
 
 chunkSize = 2000
-connection = sqlal.create_engine('mysql+mysqlconnector://michael-kofi.nyanyui:knuMANJwJYSM2Nd1@10.10.190.39/23_2L2_michael_kofi_nyanyuie')
+connection = sqlal.create_engine('mysql+pymysql://michael-kofi.nyanyui:knuMANJwJYSM2Nd1@10.10.190.39/23_2L2_michael_kofi_nyanyuie')
 
 for i, chunk in enumerate(range(0, tempDF.shape[0], chunkSize)):
     newTuples = tempDF.iloc[chunk : chunk + chunkSize]
 
-    newTuples.to_csv(f"temp2021_chunk{i+1}.csv", index=False)
-    newTuples.to_sql('TEMPERATURES', con=connection, if_exists='replace', index=False)
+    # newTuples.to_csv(f"temp2021_chunk{i+1}.csv", index=False)
+    newTuples.to_sql('TEMPERATURES', con=connection, if_exists='append', index=False)
