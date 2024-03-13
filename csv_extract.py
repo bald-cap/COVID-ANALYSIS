6ea9814a6a14c3ee59e8e668f28b77179099b7ff
MICHAEL KOFI NYANYUIE <mnyanyuie@gmail.com>
2024-03-13 18:44:49 +0100

    Update csv_extract.py

--- csv_extract.py
+++ csv_extract.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import sqlalchemy as sqlal
 
 # tempDF = pd.read_csv("temp2019.csv")
 
@@ -14,11 +13,8 @@ import sqlalchemy as sqlal
 # tempDF = pd.read_csv("temp2020/temp2020.csv")
 tempDF = pd.read_csv("temp2021.csv", sep=";")
 
-
 chunkSize = 2000
-connection = sqlal.create_engine('mysql+pymysql://michael-kofi.nyanyui:knuMANJwJYSM2Nd1@10.10.190.39/23_2L2_michael_kofi_nyanyuie')
 
-tempDF.columns = ['DATE_TEMP', 'DEP_ID', 'DEPARTMENT', 'MIN_TEMP', 'MAX_TEMP', 'MEAN_TEMP']
 for i, chunk in enumerate(range(0, tempDF.shape[0], chunkSize)):
     newTuples = tempDF.iloc[chunk : chunk + chunkSize]
 
