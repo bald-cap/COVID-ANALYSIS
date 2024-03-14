9e61527e92ef0620640a3880850a9abecf64a574
MICHAEL KOFI NYANYUIE <mnyanyuie@gmail.com>
2024-03-14 21:25:04 +0100

    better chunks

--- csv_extract.py
+++ csv_extract.py
@@ -11,11 +11,11 @@ import pandas as pd
 
 
 # tempDF = pd.read_csv("temp2020/temp2020.csv")
-tempDF = pd.read_csv("covid-hospit.csv", sep=";")
+tempDF = pd.read_csv("temperature-quotidienne-departementale.csv", sep=";")
 
 chunkSize = 50000
 
 for i, chunk in enumerate(range(0, tempDF.shape[0], chunkSize)):
     newTuples = tempDF.iloc[chunk : chunk + chunkSize]
 
-    newTuples.to_csv(f"covid_chunk{i+1}.csv", index=False)
+    newTuples.to_csv(f"temp-dept_chunk{i+1}.csv", index=False)
