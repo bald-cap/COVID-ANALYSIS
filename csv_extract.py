2522f535e30d6022ff3f9a2cce0368609ef8395c
MICHAEL KOFI NYANYUIE <mnyanyuie@gmail.com>
2024-03-14 21:15:03 +0100

    created covid table and chunks

--- csv_extract.py
+++ csv_extract.py
@@ -11,11 +11,11 @@ import pandas as pd
 
 
 # tempDF = pd.read_csv("temp2020/temp2020.csv")
-tempDF = pd.read_csv("temp2021.csv", sep=";")
+tempDF = pd.read_csv("covid-hospit.csv", sep=";")
 
-chunkSize = 2000
+chunkSize = 50000
 
 for i, chunk in enumerate(range(0, tempDF.shape[0], chunkSize)):
     newTuples = tempDF.iloc[chunk : chunk + chunkSize]
 
-    newTuples.to_csv(f"temp2021_chunk{i+1}.csv", index=False)
+    newTuples.to_csv(f"covid_chunk{i+1}.csv", index=False)
